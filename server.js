b7ed9473e1d852b494a5f88807ffeea29da2e5d5
Craig Phares <craigphares@gmail.com>
2019-03-01 12:01:02 -0500

    comments

--- server.js
+++ server.js
@@ -20,11 +20,22 @@ app.use(morgan('dev'))
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
+// default PNG size
 const DEFAULT_SIZE = 80
+
+// smallest PNG size
+const MIN_SIZE = 1
+
+// largest PNG size
 const MAX_SIZE = 1024
+
+// outer color size
 const SVG_SIZE = 32
+
+// inner icon size
 const ICON_SIZE = 24
 
+// available colors
 const COLORS = [
   '#F44336',
   '#E91E63',
@@ -44,6 +55,7 @@ const COLORS = [
   '#FF5722',
 ]
 
+// available icon names
 const ICONS = [
   'ac_unit',
   'adjust',
@@ -89,10 +101,53 @@ const ICONS = [
   'weekend',
 ]
 
-const integer = (r, min, max) => Math.floor(r() * (max - min + 1) + min)
+/**
+ * integer() generates a random integer between the minimum and maximum
+ * based on the seeded random number generator
+ *
+ * @param {seedrandom} seededRng
+ * @param {Integer} min
+ * @param {Integer} max
+ * @return {Integer} integer
+ */
+const integer = (seededRng, min, max) => Math
+  .floor(seededRng() * (max - min + 1) + min)
 
-const pickone = (r, arr) => arr[integer(r, 0, arr.length - 1)]
+/**
+ * pickone() picks a random element from an array
+ * using the seeded random number generator
+ *
+ * @param {seedrandom} seededRng
+ * @param {Array} arr
+ * @return {Object} object
+ */
+const pickone = (seededRng, arr) => arr[integer(seededRng, 0, arr.length - 1)]
 
+/**
+ * trimSize() constrains a size within a boundary
+ *
+ * @param {Integer} size
+ * @param {Integer} defaultSize
+ * @param {Integer} minSize
+ * @param {Integer} maxSize
+ * @return {Integer} integer
+ */
+const trimSize = (size, defaultSize, minSize, maxSize) => {
+  const parsedSize = Number.isNaN(size) ? defaultSize : size
+  const sizeInt = parseInt(parsedSize, 10)
+  const trimmedSize = Math.max(sizeInt, minSize)
+  return Math.min(trimmedSize, maxSize)
+}
+
+/**
+ * generateSvg() creates an SVG with a white icon over a color background
+ * based on a hash string
+ *
+ * @param {String} hash
+ * @param {Array} colors
+ * @param {Array} icons
+ * @return {String} string
+ */
 const generateSvg = (hash, colors, icons) => {
   const rng = seedrandom(hash)
   const color = pickone(rng, colors)
@@ -100,7 +155,8 @@ const generateSvg = (hash, colors, icons) => {
   let whiteIcon
   if (icon) {
     const iconPath = `./icons/baseline-${icon}-${ICON_SIZE}px.svg`
-    const iconContent = fs.readFileSync(path.resolve(__dirname, iconPath), 'utf8')
+    const iconFile = path.resolve(__dirname, iconPath)
+    const iconContent = fs.readFileSync(iconFile, 'utf8')
     const x2js = new X2JS()
     const jsonObj = x2js.xml2js(iconContent)
     const offset = (SVG_SIZE - ICON_SIZE) / 2
@@ -114,20 +170,31 @@ const generateSvg = (hash, colors, icons) => {
   } else {
     whiteIcon = ''
   }
-
   return [
-    `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 ${SVG_SIZE} ${SVG_SIZE}" version="1.1">`,
+    '<svg xmlns="http://www.w3.org/2000/svg"',
+    ' xmlns:xlink="http://www.w3.org/1999/xlink"',
+    ` style="isolation:isolate" viewBox="0 0 ${SVG_SIZE} ${SVG_SIZE}"`,
+    ' version="1.1">',
     `<path d="M0 0h${SVG_SIZE}v${SVG_SIZE}H0V0z" fill="${color}" />`,
     whiteIcon,
     '</svg>',
   ].join('')
 }
 
-const trimSize = (size) => {
-  const parsedSize = Number.isNaN(size) ? DEFAULT_SIZE : size
-  const sizeInt = parseInt(parsedSize, 10)
-  const minSize = Math.max(sizeInt, 1)
-  return Math.min(minSize, MAX_SIZE)
+/**
+ * generatePng() creates a PNG file from an SVG string at a specific size
+ *
+ * @param {*} svg
+ * @param {*} size
+ * @return {Promise} promise
+ */
+const generatePng = (svg, size) => {
+  const trimmedSize = trimSize(size, DEFAULT_SIZE, MIN_SIZE, MAX_SIZE)
+  const options = { density: 72 * trimmedSize / SVG_SIZE }
+  return sharp(Buffer.from(svg), options)
+    .resize(trimmedSize, trimmedSize)
+    .png()
+    .toBuffer()
 }
 
 const router = express.Router()
@@ -143,14 +210,8 @@ router.get('/avatar/:hash.svg', (req, res) => {
 router.get('/avatar/:hash.png', async (req, res) => {
   const { hash } = req.params
   const size = req.query.s || req.query.size || DEFAULT_SIZE
-  const trimmedSize = trimSize(size)
   const svg = generateSvg(hash, COLORS, ICONS)
-  const png = await sharp(Buffer.from(svg), {
-    density: 72 * trimmedSize / SVG_SIZE,
-  })
-    .resize(trimmedSize, trimmedSize)
-    .png()
-    .toBuffer()
+  const png = await generatePng(svg, size)
   res.status(200)
   res.setHeader('Content-Type', 'image/png')
   return res.end(png)
@@ -175,14 +236,8 @@ router.get('/avatar.svg', (req, res) => {
 router.get('/avatar.png', async (req, res) => {
   const hash = ''
   const size = req.query.s || req.query.size || DEFAULT_SIZE
-  const trimmedSize = trimSize(size)
   const svg = generateSvg(hash, COLORS, [])
-  const png = await sharp(Buffer.from(svg), {
-    density: 72 * trimmedSize / SVG_SIZE,
-  })
-    .resize(trimmedSize, trimmedSize)
-    .png()
-    .toBuffer()
+  const png = await generatePng(svg, size)
   res.status(200)
   res.setHeader('Content-Type', 'image/png')
   return res.end(png)
@@ -205,4 +260,5 @@ app.use('/', router)
 const port = process.env.PORT || 5000
 const env = app.get('env')
 // eslint-disable-next-line no-console
-app.listen(port, () => console.log(`App is listening on port ${port} in ${env} mode!`))
+app.listen(port, () => console
+  .log(`App is listening on port ${port} in ${env} mode!`))
